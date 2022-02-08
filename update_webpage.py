1887ea9b8bfc4e8423c7347a6835eb1e3710796b
mstaczek <matemmatem2000@gmail.com>
2022-02-08 17:39:21 +0100

    fixed updating webpage with script

--- update_webpage.py
+++ update_webpage.py
@@ -2,7 +2,7 @@ import shutil
 import os
 
 def main():
-    shutil.copyfile("results_2022_1.html", os.path.join("docs", "index2.html"))
+    shutil.copyfile("results_2022_1.html", os.path.join("docs", "index.html"))
 
 
 if __name__ == '__main__':
