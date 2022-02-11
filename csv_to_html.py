c9d0f12027536eabee517b9459715445bdaf5c71
mstaczek <matemmatem2000@gmail.com>
2022-02-11 16:28:26 +0100

    add basic course info

--- csv_to_html.py
+++ csv_to_html.py
@@ -18,7 +18,9 @@ def csv_to_html2(input_csv_path, output_html_path):
     env = jinja2.Environment(loader=loader)
     template = env.get_template('')
     input_df = pd.read_csv(input_csv_path, na_filter=False)
-    params = {'df':input_df, 'year':2022}
+    card_titles = input_df['What']
+    card_descriptions = input_df[['Prerequisites','Objectives','Programme']]
+    params = {'df':input_df, 'year':2022, 'card_titles':card_titles, 'card_descriptions':card_descriptions} 
     output = template.render(params)
     with open(output_html_path, "w", encoding='utf-8') as fh:
         fh.write(output)
