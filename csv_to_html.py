8f0263d81cb7dc68cb151c2887a55c6226d0050e
mstaczek <matemmatem2000@gmail.com>
2022-02-11 17:42:15 +0100

    last changes to data presentation

--- csv_to_html.py
+++ csv_to_html.py
@@ -19,10 +19,10 @@ def csv_to_html2(input_csv_path, output_html_path):
     template = env.get_template('')
     input_df = pd.read_csv(input_csv_path, na_filter=False)
     card_titles = input_df['What']
-    card_descriptions = input_df[['Prerequisites','Objectives','Programme','Exam']]
-    course_links = input_df['Link']
-    card_descriptions_details = input_df[['Professor','Other professors','Address','Tags','Might be online?','When','Code']]
-    params = {'df':input_df, 'year':2022, 'card_titles':card_titles, 'card_descriptions':card_descriptions, 'card_descriptions_details':card_descriptions_details, 'course_links':course_links} 
+    colnames_for_card_body = ['Where','Prerequisites','Objectives','Programme','Exam']
+    colname_for_link = 'Link'
+    colnames_for_card_desc = ['Min. year','Language','How','Might be online?','Tags','Professor','Other professors','Address','When','Code']
+    params = {'courses_data':input_df, 'year':2022, 'card_titles':card_titles, 'colnames_for_card_body':colnames_for_card_body, 'colnames_for_card_desc':colnames_for_card_desc, 'colname_for_link':colname_for_link} 
     output = template.render(params)
     with open(output_html_path, "w", encoding='utf-8') as fh:
         fh.write(output)
