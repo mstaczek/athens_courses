33b5e33a76efa9055ae71f2f9fcd979dad32da56
mstaczek <matemmatem2000@gmail.com>
2022-02-11 16:11:12 +0100

    new basic layout html added

--- csv_to_html.py
+++ csv_to_html.py
@@ -18,7 +18,8 @@ def csv_to_html2(input_csv_path, output_html_path):
     env = jinja2.Environment(loader=loader)
     template = env.get_template('')
     input_df = pd.read_csv(input_csv_path, na_filter=False)
-    output = template.render(df=input_df)
+    params = {'df':input_df, 'year':2022}
+    output = template.render(params)
     with open(output_html_path, "w", encoding='utf-8') as fh:
         fh.write(output)
     print("New html webpage is in " + output_html_path)
