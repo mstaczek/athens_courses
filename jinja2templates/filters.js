51c028595e29818c070817edb8de5365434baa43
mstaczek <matemmatem2000@gmail.com>
2022-02-12 13:10:58 +0100

    add js to filters

---
+++ jinja2templates/filters.js
@@ -0,0 +1,54 @@
+var data = {{ js_dict | safe }};
+var cards = document.querySelectorAll('[id^="course_card_"]');
+var forms = document.querySelectorAll('[id^="filter_"]');
+
+function get_currently_checked(form_id) {
+    var form = document.getElementById(form_id);
+    var checked = [];
+    var unchecked = [];
+    for (var i = 0; i < form.elements.length; i++) {
+        if (form.elements[i].checked) {
+            checked.push(form.elements[i].value);
+        }
+        else {
+            unchecked.push(form.elements[i].value);
+        }
+    }
+    return { 'checked': checked, 'unchecked': unchecked };
+}
+
+function get_dict_of_all_filters() {
+    var dict = {};
+    for (let form of forms) {
+        var column_name = form.id.split("_").slice(1);
+        var checked = get_currently_checked(form.id);
+        if (checked['checked'].length > 0) {
+            dict[column_name] = checked['checked'];
+        }
+    }
+    return dict;
+}
+
+function hide_cards_not_in_active_filters(dict) {
+    for (let card of cards) {
+        var card_id = card.id.split("_").pop();
+        var card_data = data[card_id];
+        var hide = false;
+        for (let key in dict) {
+            if (!dict[key].includes(card_data[key].toString())) {
+                hide = true;
+            }
+        }
+        if (hide) {
+            card.style.display = "none";
+        }
+        else {
+            card.style.display = "flex";
+        }
+    }
+}
+
+function on_filters_change() {
+    var dict = get_dict_of_all_filters();
+    hide_cards_not_in_active_filters(dict);
+}
