3b46261957a25bedaea1541127a7dbe70a0f7ef6
mstaczek <matemmatem2000@gmail.com>
2022-02-16 17:28:58 +0100

    add old table view to docs

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 from links_to_htmls import links_to_htmls
 from htmls_to_csv import htmls_to_csv
-from csv_to_html import csv_to_html_web
+from csv_to_html import csv_to_html_web, csv_to_html_table
 
 
-def create_html_webpage_from_links(links, htmls, csv, result):
+def create_html_webpage_from_links(links, htmls, csv, result, result_table):
     links_to_htmls(input_urls_txt=links, output_htmls_folder=htmls)
     htmls_to_csv(input_htmls_folder=htmls, output_csv_path=csv)
     csv_to_html_web(input_csv_path=csv, output_html_path=result)
+    csv_to_html_table(input_csv_path=csv, output_html_path=result_table)
 
 
 if __name__ == '__main__':
@@ -14,4 +15,5 @@ if __name__ == '__main__':
     htmls = "html_downloads_2022_1"
     csv = "results_2022_1.csv"
     result = "results_2022_1_web.html"
-    create_html_webpage_from_links(links, htmls, csv, result)
+    result_table = "results_2022_1_table.html"
+    create_html_webpage_from_links(links, htmls, csv, result, result_table)
--- update_webpage.py
+++ update_webpage.py
@@ -3,6 +3,7 @@ import os
 
 def main():
     shutil.copyfile("results_2022_1_web.html", os.path.join("docs", "index.html"))
+    shutil.copyfile("results_2022_1_table.html", os.path.join("docs", "table.html"))
 
 
 if __name__ == '__main__':
