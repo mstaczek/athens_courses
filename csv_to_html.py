f591159c60757549a1bf4dbb1308f958f7a68c80
mstaczek <matemmatem2000@gmail.com>
2022-02-16 17:16:34 +0100

    rename and organize files

--- csv_to_html.py
+++ csv_to_html.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import jinja2
 
-def csv_to_html(input_csv_path, output_html_path):
+def csv_to_html_table(input_csv_path, output_html_path):
     print("Creating pretty html table from csv ;)")
-    loader = jinja2.FileSystemLoader('jinja2template.html')
+    loader = jinja2.FileSystemLoader('jinja2templates/webpage/')
     env = jinja2.Environment(loader=loader)
-    template = env.get_template('')
+    template = env.get_template('jinja2template.html')
     input_df = pd.read_csv(input_csv_path, na_filter=False)
     output = template.render(df=input_df)
     with open(output_html_path, "w", encoding='utf-8') as fh:
@@ -13,9 +13,9 @@ def csv_to_html(input_csv_path, output_html_path):
     print("New html table is in " + output_html_path)
 
 
-def csv_to_html3(input_csv_path, output_html_path):
+def csv_to_html_web(input_csv_path, output_html_path):
     print("Creating pretty html webpage from csv ;)")
-    loader = jinja2.FileSystemLoader('jinja2templates/')
+    loader = jinja2.FileSystemLoader('jinja2templates/webpage/')
     env = jinja2.Environment(loader=loader)
     template = env.get_template('jinja2template3.html')
     input_df = pd.read_csv(input_csv_path, na_filter=False)
@@ -37,6 +37,6 @@ def csv_to_html3(input_csv_path, output_html_path):
     print("New html webpage is in " + output_html_path)
 
 if __name__ == '__main__':
-    # csv_to_html(input_csv_path='results_2022_1.csv', output_html_path='results_2022_1.html')
-    csv_to_html3(input_csv_path='results_2022_1.csv', output_html_path='results_2022_1_new.html')
+    csv_to_html_table(input_csv_path='results_2022_1.csv', output_html_path='results_2022_1_table.html')
+    csv_to_html_web(input_csv_path='results_2022_1.csv', output_html_path='results_2022_1_web.html')
     
--- run.py
+++ run.py
@@ -1,17 +1,17 @@
 from links_to_htmls import links_to_htmls
 from htmls_to_csv import htmls_to_csv
-from csv_to_html import csv_to_html
+from csv_to_html import csv_to_html_web
 
 
-def create_html_table_from_links(links, htmls, csv, result):
+def create_html_webpage_from_links(links, htmls, csv, result):
     links_to_htmls(input_urls_txt=links, output_htmls_folder=htmls)
     htmls_to_csv(input_htmls_folder=htmls, output_csv_path=csv)
-    csv_to_html(input_csv_path=csv, output_html_path=result)
+    csv_to_html_web(input_csv_path=csv, output_html_path=result)
 
 
 if __name__ == '__main__':
     links = "links_2022_1.txt"
     htmls = "html_downloads_2022_1"
     csv = "results_2022_1.csv"
-    result = "results_2022_1.html"
-    create_html_table_from_links(links, htmls, csv, result)
+    result = "results_2022_1_web.html"
+    create_html_webpage_from_links(links, htmls, csv, result)
