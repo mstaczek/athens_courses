8cfb1829002b8b36b4ba491cf0839e02c0f7f30f
mstaczek <matemmatem2000@gmail.com>
2023-09-09 17:48:35 +0200

    Add 2023 November autumn session

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from GenerateWebpage.csv_to_html import csv_to_html_web, csv_to_html_table
 
 
 def create_html_webpage_from_links(links, htmls, csv, result, result_table):
-    # links_to_htmls(input_urls_txt=links, output_htmls_folder=htmls)
+    links_to_htmls(input_urls_txt=links, output_htmls_folder=htmls)
     htmls_to_csv(input_htmls_folder=htmls, output_csv_path=csv)
     csv_to_html_web(input_csv_path=csv, output_html_path=result)
     csv_to_html_table(input_csv_path=csv, output_html_path=result_table)
@@ -12,7 +12,7 @@ def create_html_webpage_from_links(links, htmls, csv, result, result_table):
 
 if __name__ == '__main__':
     year = 2023
-    session_no_this_year = 1
+    session_no_this_year = 2
     links = f"GenerateCsv/links_{year}_{session_no_this_year}.txt"
     htmls = f"GenerateCsv/html_downloads_{year}_{session_no_this_year}"
     csv = f"results_{year}_{session_no_this_year}.csv"
